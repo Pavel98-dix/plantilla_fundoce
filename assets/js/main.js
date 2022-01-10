099ed0b24bff01d2dfcd6c9b575576455802d843
Aldo Pavel <aldo_vm@tesch.edu.mx>
2022-01-09 18:01:40 -0600

    Primer commit de la plantilla para la fundacion

---
+++ assets/js/main.js
@@ -0,0 +1,107 @@
+(function() {
+    "use strict";
+
+    /**
+     * Easy selector helper function
+     */
+    const select = (el, all = false) => {
+            el = el.trim()
+            if (all) {
+                return [...document.querySelectorAll(el)]
+            } else {
+                return document.querySelector(el)
+            }
+        }
+        /**
+         * Easy event listener function
+         */
+    const on = (type, el, listener, all = false) => {
+            let selectEl = select(el, all)
+            if (selectEl) {
+                if (all) {
+                    selectEl.forEach(e => e.addEventListener(type, listener))
+                } else {
+                    selectEl.addEventListener(type, listener)
+                }
+            }
+        }
+        /**
+         * Easy on scroll event listener 
+         */
+    const onscroll = (el, listener) => {
+        el.addEventListener('scroll', listener)
+    }
+
+    /**
+     * Mobile nav toggle
+     */
+    on('click', '.mobile-nav-toggle', function(e) {
+        select('#navbar').classList.toggle('navbar-mobile')
+        this.classList.toggle('bi-list')
+        this.classList.toggle('bi-x')
+    })
+
+    /**
+     * Mobile nav dropdowns activate
+     */
+    on('click', '.navbar .dropdown > a', function(e) {
+        if (select('#navbar').classList.contains('navbar-mobile')) {
+            e.preventDefault()
+            this.nextElementSibling.classList.toggle('dropdown-active')
+        }
+    }, true)
+
+
+    let heroCarouselIndicators = select("#hero-carousel-indicators")
+    let heroCarouselItems = select('#heroCarousel .carousel-item', true)
+
+    heroCarouselItems.forEach((item, index) => {
+        (index === 0) ?
+        heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "' class='active'></li>":
+            heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "'></li>"
+    });
+
+    //VIDEO POP - UP ===
+    //    === === === === === === === === === === === === === === === === === === === === === === === == * /
+    $('.video-popup').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false,
+    });
+
+    /* Nivo Lightbox
+    ========================================================*/
+    $('.lightbox').nivoLightbox({
+        effect: 'fadeScale',
+        keyboardNav: true,
+    });
+
+    //=====  Slick Showcase active
+
+    $('.showcase-active').slick({
+        dots: false,
+        infinite: true,
+        speed: 600,
+        slidesToShow: 1,
+        arrows: true,
+        prevArrow: '<span class="prev"><i class="bi bi-arrow-left"></i></span>',
+        nextArrow: '<span class="next"><i class="bi bi-arrow-right"></i></span>',
+        adaptiveHeight: true,
+        responsive: [{
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    arrows: false,
+                }
+            }
+        ]
+    });
+})()
